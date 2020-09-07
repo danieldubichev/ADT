76c4203a7eb97ab4a01d19d4367da908f3325022
danieldubichev <dannydubichev@gmail.com>
2020-09-07 16:42:19 -0700

    added logic / explanations for algorithm

--- Linked Lists/Classic Problems/Java/Flatten/Flatten.java
+++ Linked Lists/Classic Problems/Java/Flatten/Flatten.java
@@ -19,20 +19,34 @@ public class Flatten {
 
             //if child found
             //find child tail
-            Node temp = p.child;
+            Node temp = pointer.child;
             while(temp.next != null){
                 temp = temp.next;
             }
             //connect the tail with pointer next value if it is not null
+            System.out.println(pointer.next.val);
+            //in the test case the end of our tail (10) point to 4 now
             temp.next = pointer.next;
+            
             if (pointer.next != null){
                 pointer.next.prev = temp;
+                //4 now points back to 10
+                System.out.println(pointer.next.prev.val);
             }
             pointer.next = pointer.child;
+            //the pointer (3) is now pointing to the beginning of the child
+            System.out.println(pointer.next.val);
             pointer.child.prev = pointer;
+            //7 now points to 3 (prev)
+            System.out.println(pointer.next.val);
+            System.out.println(pointer.val);
+            //the child can be null now as it is transferred to the main flattenned list!
             pointer.child = null;
+            //now repeat the same process for all future child values!
         }
 
         return head;
     }
 }
+
+
